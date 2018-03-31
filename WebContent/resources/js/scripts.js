b605cc77fc43934cfeeb19def7e4eea696b297f6
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-03-31 12:17:57 -0300

    resolvido bug validação javascript dialog novo campo required + efeito

--- WebContent/resources/js/scripts.js
+++ WebContent/resources/js/scripts.js
@@ -1,7 +1,8 @@
-function verificaCampo(xhr, status, args, dlg) {
+function verificaCampo(xhr, status, args, dlg, tbl) {
 	if (args.validationFailed) {
 		PF(dlg).jq.effect("shake", {times : 5}, 100);
 	} else {
 		PF(dlg).hide();
+		PF(tbl).clearFilters();
 	}
 }
